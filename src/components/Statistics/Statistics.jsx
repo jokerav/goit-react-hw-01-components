58bad873bbc96c99871d0edb0b7001378f7202cb
jokerav <50701756+jokerav@users.noreply.github.com>
2022-04-20 19:59:32 +0300

    добавил key

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,22 +1,22 @@
 import PropTypes from 'prop-types';
-import s from './Statistics.module.css'
-export default function Statistics({ title, stats })
-{
-    return (
-        <section className={s.statistics}>
-            {title ? <h2 className={s.title}>Upload stats</h2>:''}
-        <ul className={s.statList}>
-        {  stats.map(data =>
-          <li className={s.item}>
-            <span className={s.label}>{data.label}</span>
-            <span className={s.percentage}>{data.percentage}</span>
-          </li>) }
-        </ul>
-</section>   
-)}
-
-Statistics.propTypes = { 
-  title: PropTypes.string,
-  stats: PropTypes.array
-
+import s from './Statistics.module.css';
+export default function Statistics({ title, stats }) {
+  return (
+    <section className={s.statistics}>
+      {title ? <h2 className={s.title}>Upload stats</h2> : ''}
+      <ul className={s.statList}>
+        {stats.map(({ id, label, percentage }) => (
+          <li className={s.item} key={id}>
+            <span className={s.label}>{label}</span>
+            <span className={s.percentage}>{percentage}</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
 }
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.array,
+};
